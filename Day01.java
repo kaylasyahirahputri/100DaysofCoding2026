5746cf0d787c0734f72b6c570bf66700726d16df
kaylasyahirahputri <kaylasyahirahputri06@gmail.com>
2026-09-02 22:55:03 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,10 @@
+public class day01 {
+  public static void main (String[] args) {
+    // Komentar tipe 1: cukup satu baris saja
+         System.out.println("Belajar struktur dasar Java");
+         /*
+            Komentar tipe 2: Bisa menampung banyak baris
+         */
+         System.out.println("Commit hari pertama saya");
+     }
+    }
