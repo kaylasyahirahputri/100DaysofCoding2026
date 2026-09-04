93c46cb568ddd6b65957beb80b906c16e234f8ea
kaylasyahirahputri <kaylasyahirahputri06@gmail.com>
2026-09-04 13:55:09 +0700

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,27 @@
+public class day03 {
+    public static void main(String[] args) {
+        // Menggunakan 4 Escape Sequence: \n, \t, \", \\
+
+        // \" Double Quote
+        System.out.println("\"=== PROFIL MAHASISWA ===\"") ;
+        
+        // \n New Line 
+        System.out.println("Nama : Kayla Syahirah Putri \nNIM : D0226002") ;
+        
+        // \t Tab
+        System.out.print("Prodi\t: Informatika") ;
+        System.out.println() ;
+        System.out.print("Fakultas\t: Teknik") ;
+        System.out.println() ;
+        System.out.print("Semester\t: 1") ;
+        System.out.println() ;
+        System.out.print("Kampus\t: Universitas Sulawesi Barat") ;
+        System.out.println() ;
+
+        // \\ Blacklash
+        System.out.println("Backslash\t: \\") ;
+
+    }
+}
+
+
