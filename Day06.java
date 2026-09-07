d186b8f1d8289567bc1bda5f1490d650e62e817e
kaylasyahirahputri <kaylasyahirahputri06@gmail.com>
2026-09-07 11:29:33 +0700

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,29 @@
+public class day06 {
+    public static void main (String[] args) {
+        /*Tipe Data Karakter char dan boolean*/
+
+        /* Tipe data char adalah tipe data yang digunakan untuk
+           menyimpan satu karakter yang berupa huruf, angka, atau simbol */
+           // contoh: 
+        char grade = 'A'; //penulisannya menggunakan tanda petik satu ('').
+
+        /* Tipe data boolean adalah tipe data yang digunakan untuk
+           menyimpan logika, yaitu true (benar) atau false (salah). */
+           // contoh:
+        boolean aktif = true;
+
+        // String digunakan untuk menyimpan nama mahasiswa
+        String nama = "Kayla";
+
+        System.out.println("============================");
+        System.out.println("      DATA MAHASISWA");
+        System.out.println("============================");
+        System.out.println();
+        System.out.println("Nama   : " + nama);
+        System.out.println("Grade  : " + grade);
+        System.out.println("Aktif  : " + aktif);
+        System.out.println();
+        System.out.println("============================");
+
+    }
+}
