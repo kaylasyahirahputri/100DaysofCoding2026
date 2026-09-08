e9c10af8038752ece7201e0ebe38aa2b088cd416
kaylasyahirahputri <kaylasyahirahputri06@gmail.com>
2026-09-08 19:26:01 +0700

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,31 @@
+public class day07 {
+    public static void main(String[] args) {
+        
+        /* Tipe Data String digunakan untuk menyimpan kumpulan teks atau karakter dalam pemrograman.
+           String dapat berisi huruf, angka, kalimat, atau kombinasi dari semuanya */
+
+        String nama = "Kayla Syahirah Putri";
+        String nim = "D0226002";
+        String fakultas = "Teknik";
+        String jurusan = "Informatika";
+        String kampus = "Universitas Sulawesi Barat";
+        String alamat = "Wonomulyo";
+        String hobi = "Menggambar";
+
+        // Menampilkan biodata mahasiswa
+        System.out.println("============================");
+        System.out.println("      BIODATA MAHASISWA");
+        System.out.println("============================");
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " +  nim);
+        System.out.println("Fakultas : " + fakultas);
+        System.out.println("Jurusan : " + jurusan);
+        System.out.println("Kampus : " + kampus);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Hobi : " + hobi);
+        System.out.println();
+        System.out.println("=============================");
+
+    }
+}
